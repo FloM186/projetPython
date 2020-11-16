301d5dbedc8093a2f3e253b70288e894a9dd4f7a
mtantaoui <55891508+mtantaoui@users.noreply.github.com>
2020-11-16 16:55:18 +0100

    Histogramme variables numérique + commentaire code

--- projet.py
+++ projet.py
@@ -1,5 +1,6 @@
 import os
 import io
+from numpy.lib.shape_base import column_stack
 import pandas as pd 
 import numpy as np
 
@@ -21,9 +22,6 @@ columns = []
 # description des jeux de données 
 df_info = PreText(text='', width=400)
 df_describe = PreText(text='', width=400)
-
-# outil pour la selection de la colonne cible
-var_cible_select = Select(title="Sélectionner la variable cible :", options = [])
 
 # initialisation des boutons pour le FileDialog
 file_input = FileInput(accept=".csv")
@@ -53,16 +51,20 @@ def update_df_display(df):
     # CallBack de la selection de la variable cible
     var_cible_select.options = get_column_list(df)
 
-    # CallBack des Selects du nuage de points 
-    set_select(df)
+    # CallBack des Selects du nuage de points (mise a jour des variables)
+    nuage_var_select(df)
+
+    # CallBack des Selects du nuage de points (mise a jour des variables)
+    hist_quali_var_select(df)
 
 # fonction qui retourne les colonnes du dataset
 def get_column_list(df):
-		column_list=[]
+    column_list=[]
+    for i in df.columns:
+    	column_list.append(i)
+    # column_list.insert(0,'----------')
+    return column_list
 
-		for i in df.columns:
-			column_list.append(i)
-		return column_list
 
 file_input.on_change('filename', lambda attr, old, new: update())
 
@@ -70,36 +72,67 @@ data_table = DataTable( source=source, columns = columns,
                          width=900, height=250, sortable=True, 
                          editable=True, fit_columns=True, selectable=True )
 
-# Nuage de points
-y_select = Select(title="Ordonnées :", options = [])
-x_select = Select(title="Abcisses :", options = [])
+# outil pour la selection de la colonne cible
+var_cible_select = Select(title="Sélectionner la variable cible :", options = [])
+
+# Nuage de points------------------------------------------------------------------
+# selection des axes abscisse et ordonnées
+y_nuage_select = Select(title="Ordonnées :", options = [])
+x_nuage_select = Select(title="Abcisses :", options = [])
+
 # donnees du nuage de points 
 source_nuage = ColumnDataSource(data=dict(x=[], y=[]))
+
+# figure du nuage de points
 nuage = figure(plot_width=900, plot_height=300)
 nuage.circle(x='x', y='y', source=source_nuage)
 
-# controle du nuage de points
-controls_nuage = [x_select,y_select]
+# controle du nuage de points (appels CallBacks)
+controls_nuage = [x_nuage_select,y_nuage_select]
 for control_nuage in controls_nuage : 
     control_nuage.on_change('value',lambda attr,old,new : update_nuage())
 
 # CallBack des options des selects pour le nuage de points
-def set_select(df) :
-    y_select.options = get_column_list(df.select_dtypes(include=['float64','int64']))
-    x_select.options = get_column_list(df.select_dtypes(include=['float64','int64']))
-
+def nuage_var_select(df) :
+    y_nuage_select.options = list(np.append(['------'],get_column_list( df.select_dtypes(include=['float64','int64']))))
+    x_nuage_select.options = list(np.append(['------'],get_column_list( df.select_dtypes(include=['float64','int64']))))
 # CallBack du nuage de points 
 def update_nuage():
     df = pd.read_csv(os.path.abspath(file_input.filename))
-    source_nuage.data = dict(x=df[x_select.value],y=df[y_select.value])
+    source_nuage.data = dict(x=df[x_nuage_select.value],y=df[y_nuage_select.value])
+# ----------------------------------------------------------------------------------
 
-# Boite à moustaches
-bm = figure(plot_width=900, plot_height=300)
-bm.line()
+
+# Histogramme numérique------------------------------------------------------------------------
+# selection de la variable numérique pour l histogramme 
+hist_quanti_select = Select(title="Choisir une variable :", options = [])
+
+# donnees pour histogramme 
+source_hist_quanti= ColumnDataSource(data=dict(x=[], top=[]))
+
+# figure de l histogramme
+hist_quanti = figure(plot_width=900, plot_height=300)
+hist_quanti.vbar(x='x', top='top', source=source_hist_quanti, width=0.9)
+
+hist_quanti_select.on_change('value',lambda attr,old,new : update_hist_quali())
+
+# CallBack des options des selects pour l histogramme numérique 
+def hist_quali_var_select(df) :
+    hist_quanti_select.options = list(np.append(['------'],get_column_list( df.select_dtypes(include=['float64','int64']))))
+
+# CallBack du nuage de points 
+def update_hist_quali():
+    df = pd.read_csv(os.path.abspath(file_input.filename))
+
+    df_objects = df[ get_column_list(df.select_dtypes(include=['float64','int64'])) ]
+    
+    unique_elements, count_elements = np.unique(df_objects[hist_quanti_select.value], return_counts=True)
+    source_hist_quanti.data = dict(x=unique_elements, top=count_elements)
+# -----------------------------------------------------------------------------------------------
 
 # affichage de l'application
-scatter = Panel( child=Column( y_select, x_select, nuage ) , title='Nuage de points' )
-boxplot = Panel(child=bm , title='Histogramme')
+scatter = Panel( child=Column( y_nuage_select, x_nuage_select, nuage ), title='Nuage de points' )
+boxplot = Panel(child=Column( hist_quanti_select,hist_quanti ), title='Histogramme des variables numériques')
 tabs = Tabs(tabs=[scatter,boxplot])
 
 controls = column(file_input,df_info, var_cible_select)
