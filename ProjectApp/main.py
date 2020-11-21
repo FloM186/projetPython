9a3679e436c04ceabef1ddc11b0ae082f331e3ad
Florian <moretflorian@gmail.com>
2020-11-22 00:17:33 +0100

    Update main.py

--- ProjectApp/main.py
+++ ProjectApp/main.py
@@ -384,30 +384,13 @@ strategy_imputer_reg_lin = Select(title='Stratégie de remplacement des valeurs 
 #res_lr = PreText(text='', width=900, height=700)
 res_summ = PreText(text='', width=400)
 tableau_alpha =  PreText(text='', width=400)
-
+lin_mse = PreText(text='', width=400)
+resu_summ = PreText(text='', width=400)
 
 # Variables de la regression linéaire
 controls_reg_lin = [var_cible_reg_lin_select,var_pred_reg_lin_choice, strategy_imputer_reg_lin, slider_reg_lin_train_test]
 for control_reg_lin in controls_reg_lin:
     control_reg_lin.on_change('value', lambda attr,old,new: update_reg_lin())
-
-
-# learnig curve pour la regression logistique 
-source_learn_curve = ColumnDataSource(data=dict(train_sizes=[], train_mean=[],  
-                                                train_mean_p_train_std=[],
-                                                train_mean_m_train_std=[],
-                                                test_mean=[],
-                                                test_mean_p_test_std=[],
-                                                test_mean_m_test_std=[]))
-learn_curve = figure(title="Accuracy", title_location="left", plot_width=600, plot_height=400)
-learn_curve.add_layout(Title(text="number of training examples", align="center"), "below")
-learn_curve.varea('train_sizes', 'train_mean_p_train_std','train_mean_m_train_std', source = source_learn_curve, fill_color= "lightskyblue")
-learn_curve.line( 'train_sizes', 'train_mean', source = source_learn_curve )
-learn_curve.circle( 'train_sizes', 'train_mean', source = source_learn_curve, size = 15, fill_color='blue', legend_label='training accuracy')
-learn_curve.varea('train_sizes', 'test_mean_p_test_std','test_mean_m_test_std', source = source_learn_curve, fill_color= "palegreen")
-learn_curve.line('train_sizes', 'test_mean', source=source_learn_curve)
-learn_curve.circle( 'train_sizes', 'test_mean', source = source_learn_curve, size = 15, fill_color='green', legend_label='validation accuracy')
-
 
 
 # CallBack des features de la regression linéaire
@@ -435,17 +418,17 @@ def update_reg_lin():
     linreg = sm.OLS(y_train,X_train)
     res = linreg.fit()
 
-    #res_summ.text = str(res.summary())  
+    res_summ.text = str(res.summary())  
 
     #prediction régression linéaire 
     ypred = linreg.predict(res.params,X_test)
 
     # figure du nuage de points
-    plotprediction = figure(plot_width=900, plot_height=300)
-    plotprediction.circle(x=y_test, y=ypred)
+    #plotprediction = figure(plot_width=900, plot_height=300)
+    #plotprediction.circle(x=y_test, y=ypred)
 
     #calcule MSE
-    lin_mse = mean_squared_error(y_test,ypred)
+    lin_mse.text = mean_squared_error(y_test,ypred)
 
    
 
@@ -457,6 +440,7 @@ def update_reg_lin():
     
     linregu = sm.OLS(y_trainsc,X_trainsc)
     resu = linregu.fit()
+    resu_summ.text = str(resu.summary())  
     #model = linregu
     #results_fu = resu
     frames = []
@@ -476,89 +460,10 @@ def update_reg_lin():
     tableau_alpha.text = str(tableau_alpha)
     
     #graphiques
-    %matplotlib inline
-
-    fig, ax = plt.subplots(1, 2, figsize=(14, 4))
-
-    ax[0] = df6.iloc[:, :-1].plot(ax=ax[0])
-    ax[0].set_title('Coefficient')
-
-    ax[1] = df6.iloc[:, -1].plot(ax=ax[1])
-    ax[1].set_title('SSR')
-
-
-
-
-
-
-
-# CallBack des features de la regression logistique 
-def update_reg_log():
-    # la source de données pour la regression logistique 
-    df = pd.read_csv(join(dirname(__file__), 'datasets/'+file_input.filename))
-
-    # la variable cible de la regression logistique
-    y = df[var_cible_reg_log_select.value].values
-
-    # les variables cibles de la regression logistique 
-    X = df[var_pred_reg_log_choice.value].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=slider_reg_log_train_test.value,
-                                                        random_state=1, stratify=y)  
-
-    # encodage des variables cibles categorielles qui ne sont pas numerique
-    labelEncoder_y = LabelEncoder()
-    if(y.dtype == 'object'):
-        y_train = labelEncoder_y.fit_transform(y_train)
-        y_test = labelEncoder_y.fit_transform(y_test)
-    
-    # traitement des valeurs manquantes 
-    imputer = SimpleImputer(missing_values=np.nan, strategy = strategy_imputer_reg_log.value)
-    imputer = imputer.fit(X_train)
-    X_train = imputer.transform(X_train)
-    imputer = imputer.fit(X_test)
-    X_test = imputer.transform(X_test)
-
-    # regression logistique avec statsmodels
-    lr = MNLogit(endog=np.int64(y_train), exog=X_train)
-    res = lr.fit()
-    
-    model = LogisticRegression(random_state=0, multi_class='multinomial', penalty='none', solver='newton-cg').fit(X_train, y_train)
-    y_pred = model.predict(X_test)
-    
-    matrix_conf = pd.DataFrame( np.array(confusion_matrix(y_test, y_pred)), index=np.unique(y), columns=[str(i) for i in np.unique(y)] )
-    matrix_conf.insert(0, "Observations\Prédictions", np.unique(y), True) 
-    
-    class_report=classification_report(y_test, y_pred)
-    res_lr.text = str(res.summary())+'\n'+str(class_report)+'\n Accuracy score : '+str(accuracy_score(y_test, y_pred))
-
-    # CallBack des colonnes (TableColumn) pour l affichage de la table (DataTable) 
-    source_conf.data = {matrix_conf[column_name].name : matrix_conf[column_name] for column_name in get_column_list(matrix_conf)}
-    data_conf.source = source_conf
-    data_conf.columns = [TableColumn(field = matrix_conf[column_name].name, title = matrix_conf[column_name].name, editor = StringEditor()) for column_name in get_column_list(matrix_conf)]
-
-    # # validation croisee stratifiée
-    pipe_lr = make_pipeline(StandardScaler(),
-                        LogisticRegression(penalty='l2', random_state=1,
-                                            solver='lbfgs', max_iter=100000))
-    train_sizes, train_scores, test_scores = learning_curve(estimator=pipe_lr,
-                               X=X_train,
-                               y=y_train,
-                               train_sizes=np.linspace(0.1, 1.0, 10),
-                               cv=10,
-                               n_jobs=1)
-    
-    train_mean = np.mean(train_scores, axis=1)
-    train_std = np.std(train_scores, axis=1)
-    test_mean = np.mean(test_scores, axis=1)
-    test_std = np.std(test_scores, axis=1)
-
-    source_learn_curve.data = dict( train_sizes=train_sizes, train_mean=train_mean,
-                                    train_mean_p_train_std=(train_mean+train_std/3), 
-                                    train_mean_m_train_std=(train_mean-train_std/3),
-                                    test_mean_p_test_std=(test_mean+test_std/6),
-                                    test_mean_m_test_std=(test_mean-test_std/6),
-                                    test_mean=test_mean )
-
+    #%matplotlib inline
+    #fig, ax = plt.subplots(1, 2, figsize=(14, 4))
+    #ax[0] = df6.iloc[:, :-1].plot(ax=ax[0])
+    #ax[0].set_title('Coefficient')
 
 # Fin de la regression linéaire---------------------------------------------------------------------------------- 
 
@@ -582,6 +487,13 @@ logist = Panel( child= Column(Row( var_cible_reg_log_select, var_pred_reg_log_ch
                                 Row(penalty_lr_select, spinner_c_lr, spinner_cv_lr),
                                 data_conf,roc_curve_lr,learn_curve_lr, res_lr ), title='Régression Logistique' )
 
+
+#affichage regression linéaire
+reglineaire = Panel( child= Column(Row( var_cible_reg_lin_select, var_pred_reg_lin_choice), 
+                                Row(slider_reg_lin_train_test, strategy_imputer_reg_lin),
+                                Row(slider_reg_lin_alpha, slider_reg_lin_alpha_pas),
+                                res_summ,tableau_alpha ,lin_mse, resu_summ), title='Régression Linéaire' )
+#plotprediction
 
 SVM = Panel( child=Row(), title='SVM' )
 
